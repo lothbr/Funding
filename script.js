dcf1e563ce2f3f127413dc1597891752eefd330e
lothbr <39369616+lothbr@users.noreply.github.com>
2023-11-05 22:33:51 +0100

    Update script.js

--- script.js
+++ script.js
@@ -4,12 +4,12 @@ const publicKey = 'pk_live_af141b83a9c3e559e4b5cf22fd24baa58294a3f3';
 
 // Handle donation button click
 document.getElementById('donateButton').addEventListener('click', function() {
-    const amount = document.getElementById('amount').value;
+   // const amount = document.getElementById('amount').value;
 
     const handler = PaystackPop.setup({
     key: publicKey,
     email: 'aremudaramola5@gmail.com', 
-    amount: amount * 100, 
+    amount: document.getElementById('amount').value  * 100, 
     ref: 'donation_' + Math.floor((Math.random() * 1000000000) + 1),
     onClose: function() {
         alert('Donation window closed.');
